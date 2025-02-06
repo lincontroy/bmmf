ca224672497baea35b0905919df7db3b1573e797
lincontroy <lincolnmunene37@gmail.com>
2025-02-06 11:51:48 +0300

    first commit

--- resources/views/customer/auth/register.blade.php
+++ resources/views/customer/auth/register.blade.php
@@ -83,6 +83,8 @@
          <div class="invalid-feedback">Passwords must match.</div>
       </div>
    </div>
+
+   <p style="color:black">Already have an account? <a href="{{url('customer/login')}}"> Login</a> </p>
    <div class="d-flex justify-content-center mt-4">
       <button class="btn btn-primary" type="submit">Register</button>
    </div>
