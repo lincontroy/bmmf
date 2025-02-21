4acf5b16af3149c7db491c6b82b6b35c2e5b61e5
lincontroy <lincolnmunene37@gmail.com>
2025-02-21 12:29:52 +0300

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -781,6 +781,21 @@
     <!-- End Back To Top -->
 
 
+     <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67b744d10429ca1916ecc060/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+
     <!--   JQUERY FILES     -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
